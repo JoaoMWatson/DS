33367b60e9e4e3e147cd8eecd6e2af8aa93e0cb5
Joao Pedro <joaopedrowatson@hotmail.com>
2019-08-13 21:26:43 -0300

    coisas dessa aula

---
+++ Exemplo01.java
@@ -0,0 +1,11 @@
+public class Exemplo01 {
+
+    public static void main(String[] args) {
+        escreverNome();
+        
+    }
+    public static void escreverNome(){
+        
+    }
+
+}
---
+++ Exemplo02.java
@@ -0,0 +1,11 @@
+public class Exemplo02{
+    public static int num1;
+
+    public static void main(String[] args) {
+        Exemplo01.escreverNome();
+        
+        int num2=0, resp=0; // Iniciar variaveis
+            num2=100;
+            resp=num1+num2;
+    }
+}
